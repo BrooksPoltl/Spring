69814a55af5ea68b6e32545b0cc7ccea78e92c83
BrooksPoltl <bpoltl1@gmail.com>
2019-08-06 11:49:34 -0500

    testing proto

--- spring-demo-one/src/com/brooks/springdemo/BeanScopeDemoApp.java
+++ spring-demo-one/src/com/brooks/springdemo/BeanScopeDemoApp.java
@@ -1,10 +1,27 @@
 package com.brooks.springdemo;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class BeanScopeDemoApp {
 
 	public static void main(String[] args) {
 		// load spring config
-		// retrieve beans
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+		
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+		
+		Coach alphaCoach = context.getBean("myCoach", Coach.class);
+		// retrieve bean
+		
+		// check if they are the same
+		boolean result = (theCoach == alphaCoach);
+		System.out.println("\n Pointing to the same object: " + result);
+		System.out.println("\n Memory location for theCoach: " + theCoach);
+		System.out.println("\n Memory location for theCoach: " + alphaCoach);
+		
+		// close context
+		context.close();
 	}
 
 }
